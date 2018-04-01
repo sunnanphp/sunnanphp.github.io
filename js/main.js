d3d7d1024409e325872ee06c770a3cd8060a6fe3
Fade <sunnanphp@gmail.com>
2018-04-01 15:41:51 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
   var json = ['你竟然点开了!','哈哈哈','恭喜你，你被骗了','知道今天是什么时间吗？','今天是4月1号','愚人节','愚人节快乐','写这么多就是想让你开心一下','你经历过绝望吗？','哈哈哈'];
-  function yuren(n){
-    if(n--){
+  function foo(){
       for(var key in json)
       {
         alert(json[key]);
       }
-    }
   }  
-  yuren(1000);
+  function test(n)
+  {
+    if(n--){
+       foo();
+    }
+  }
+  
+  test(1000);
   
   $('a.blog-button').click(function() {
     // If already in blog, return early without animate overlay panel again.
